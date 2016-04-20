88a6fad081ee4e92f8eba048b369d9f63dea0e72
eltonrafaelmelo <elton.rafaelmelo@gmail.com>
2016-04-19 21:42:45 -0300

    add gridView in project

--- app/src/main/java/com/example/eltonmelo/videos/HomeActivity.java
+++ app/src/main/java/com/example/eltonmelo/videos/HomeActivity.java
@@ -5,68 +5,65 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ItemClick;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_home)
+import Adapter.ImageAdapter;
+
+@EActivity(R.layout.home_grid_view)
 @OptionsMenu(R.menu.menu_home)
 public class HomeActivity extends AppCompatActivity {
 
     @ViewById
     Toolbar toolbar;
 
+    @ViewById
+    GridView gridview1;
+
+    ImageAdapter imageAdapter;
+
 
     @AfterViews
     void AfterViews(){
 
         setSupportActionBar(toolbar);
+        imageAdapter = new ImageAdapter(this);
+
+        gridview1.setAdapter(imageAdapter);
 
     }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @OptionsItem(R.id.action_settings)
     void myMethod() {
         // You can specify the ID in the annotation, or use the naming convention
     }
+
+    @OptionsItem(R.id.action_ordenar_por_data)
+    void ordenarPorData() {
+        // You can specify the ID in the annotation, or use the naming convention
+    }
+
+    @OptionsItem(R.id.action_ordenar_por_Favorito)
+    void ordenarPorFavorito() {
+        // You can specify the ID in the annotation, or use the naming convention
+    }
+
+    @ItemClick
+    public void gridview1(int item){
+        Toast.makeText(HomeActivity.this, "" + item,
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
